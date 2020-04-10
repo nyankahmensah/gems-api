15f139fe28575aa32faab22d1dd1b599ec878a7f
Raaj <raaj.ahab@gmail.com>
2020-04-10 14:34:53 +0000

    feat: Removes useless code

--- src/app.js
+++ src/app.js
@@ -2,13 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const path = require("path");
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
 
 module.exports = app;
--- src/rest/routes/index.js
+++ src/rest/routes/index.js
@@ -1,12 +1,5 @@
 const router = require("express").Router();
-const path = require("path");
 const { forecastController } = require("../controllers");
-
-// Web Dashboard
-// Handle React routing, return all requests to React app
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
 
 // Forecast
 router.post("/", forecastController.receiveForecast);
