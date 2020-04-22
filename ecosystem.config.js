e34cbe56269c5716b0b6bac3bfbeef2dc25e5e02
Raaj <raaj.ahab@gmail.com>
2020-04-22 01:06:59 +0000

    feat: adds image

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
         DATABASE_URI: "mongodb://localhost:27017/gmes",
-        PORT: 5427,
+        PORT: 5000,
         JWT_SECRET_KEY:
           "908u9y7gf382fho3487hf98o28l3igydeihof32uyugklhf3iogy2fuh2398o87gy3fi2up9h8ogy3f2p",
         FILE_DIRECTORY: "/home/pfz_user/.gmes"
