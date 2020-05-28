6f625e8189f0bf3d39fa07a525af49561defe261
Raaj <raaj.ahab@gmail.com>
2020-05-28 11:02:00 +0000

    feat: adds nigeria USSD support

--- src/rest/routes/index.js
+++ src/rest/routes/index.js
@@ -4,6 +4,11 @@ const { forecastController } = require("../controllers");
 // Forecast
 router.post("/", forecastController.receiveForecast);
 router.post("/ussd", forecastController.broadcastForecast);
+router.post("/ussd-nigeria", async (req, res) => {
+    console.log(req.body);
+    const response = "CON 1. Welcome to GMES"
+    return res.status(200).send(response);
+});
 router.get("/ivr-forecast", forecastController.broadcastForecastToIVR);
 router.post("/message", forecastController.receiveForecast);
 router.post("/pfz", forecastController.receivePFZ);
